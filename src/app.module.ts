f88f4b907a6b5019153edd87c65754ad3a418c6b
META³ <95690201+metacubic@users.noreply.github.com>
2022-07-05 00:24:59 +0100

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigController } from './config/config.controller';
 import { SwaggerController } from './config/swagger/swagger.controller';
 import { ormConfig } from './orm.config';
 import { UsersModule } from './api/users/users.module';
@@ -11,12 +10,15 @@ import { AuthModule } from './security/auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['./env/.env.development', './env/.env.local'],
+    }),
     TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
     UsersModule,
     AuthModule,
   ],
-  controllers: [AppController, ConfigController, SwaggerController],
+  controllers: [AppController, SwaggerController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, Logger, ValidationPipe } from '@nestjs/common';
-require('dotenv').config({ path: '.env' });
+require('dotenv').config({ path: './env/.env' });
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { setupSwagger } from './config/swagger/swagger.controller';
